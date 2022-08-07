d3502461e127aab794f914fba99be0449f8dab78
Horacio Lerda <horacio.lerda@gmail.com>
2022-08-07 10:39:34 -0300

    configuracion de lista de palabras

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
+
+
 window.onload = function(){
-    // var xAxis = [
-    //     ['01','02','03','04','05','06'],
-    //     ['07','08','09','10','11','12'],
-    //     ['13','14','15','16','17','18'],
-    //     ['19','20','21','22','23','24'],
-    //     ['25','26','27','28','29','30'],
-    //     ['31','32','33','34','35','36']
-    // ];
 
-    // for (var i = 0; i < 6; i++) {
-    //     for (var e = 0; e < 6; e++){
-    //         console.log(xAxis[i][e]);
-    //     }        
-    //   }
-
+    /*ARREGLO PARA IDENTIFICAR COLORES ASIGNADOS A LOS LETRAS INTRODUCIDAS*/
     var colores = [
         [0,0,0,0,0],
         [0,0,0,0,0],
@@ -23,13 +12,62 @@ window.onload = function(){
         [0,0,0,0,0]
     ];
 
+
+    /*ARREGLO DE LAS LETRAS INTRODUCIDAS POR EL JUGADOR*/
+    var letras = [
+        [' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ']
+    ];
+
+    var palabraGanadora = '';
+
+    var palabraFila = '';
+
+    /*ARREGLO DONDE DE DESGLOZAN LAS LETRAS DE LA PALABRA GANADORA*/
+    var arrayPalabraGanadora = ['a','b','a','c','o'];
+
+    /*ARREGLO DONDE DE DESGLOZAN LAS LETRAS DE LA PALABRA INTRODUCIDA POR EL JUGADOR*/
+    var arrayFila = [' ',' ',' ',' ',' ']; 
+
+    /*INDICA EN QUE FILA SE ENCUENTRA EL JUGADOR*/
+    var focoFila = 0;
+
+    /* --OBTENER ARRAY DE PALABRAS ELEGIR PALABRA GANADORA-- */
+    var arrayListaPalabras = Array();
+
+    fetch('https://wordle.danielfrg.com/words/5.json')
+    .then(data => {
+        return data.json();
+    })
+    .then(response => {
+        for (let i = 0; i < response.length; i++) {
+            arrayListaPalabras[i] = response[i];
+            // console.log(arrayListUsers[i])
+        }
+        let random = Math.floor(Math.random() * response.length);    
+        palabraGanadora = arrayListaPalabras[random]
+        console.log(palabraGanadora)
+        cargarArrayPalabraGanadora()
+    })
+    .catch(function(error){
+        console.log("Error", error);
+        // alert("ERROR" +'\n'+ error);
+    })
+    /* --OBTENER ARRAY DE PALABRAS ELEGIR PALABRA GANADORA-- */
+
+
+    /*CAMBIA EL COLOR DEL BLOQUE DEPENDIENDO DE SI ES CORRECTA LA LETRA Y SU POSICION*/
     function pintarTablero(){
         for (var iFila = 0; iFila < 6; iFila++) {
             for (var iColumna = 0; iColumna < 5; iColumna++){
-              const input = document.getElementById('block'+iFila+'_'+iColumna)
+                const input = document.getElementById('block'+iFila+'_'+iColumna)
             //   const input = document.getElementById('block1_1')
             //   console.log(iFila,iColumna)
-              switch (colores[iFila][iColumna]) {
+                switch (colores[iFila][iColumna]) {
                 case 0:
                         input.style.backgroundColor = 'white';
                     break;
@@ -42,33 +80,15 @@ window.onload = function(){
                 case 3:
                     input.style.backgroundColor = 'grey';
                 break;
-              
+                
                 default:
                     break;
-              }
+                }
             }        
-          } 
+            } 
     }
-
-    
-
-    var letras = [
-        [' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' ']
-    ];
-
-    var arrayPalabra = ['a','b','a','c','o'];
-
-    var arrayFila = [' ',' ',' ',' ',' ']; 
-
-    var focoFila = 0;
-
-    
-    /*Funcion que enfoca en la sigueinte fila y desabilita la anterior*/
+        
+    /*FUNCION PARA ENFOCAR EN LA SIGUEINTE FILA Y DESABILITAR LA ANTERIOR*/
     function saltarFila(){
         for(let i = 0; i < 6; i++){
             if (focoFila != i){
@@ -84,6 +104,7 @@ window.onload = function(){
 
     saltarFila();
 
+    /*MUEVE EL CURSOR AL BLOQUE SIGUEINTE CON CADA LETRA INTRODUCIDA Y CONDICIONA BACKSPACE*/
     function saltar(){
         for (let iFila = 0; iFila < 6; iFila++) {
             for (let iColumna = 0; iColumna < 5; iColumna++){
@@ -120,18 +141,18 @@ window.onload = function(){
         }
     }
 
-    function inicio(){
-        for (let iFila = 0; iFila < 6; iFila++) {
-            for (let iColumna = 0; iColumna < 5; iColumna++){
-                document.getElementById('block'+iFila+'_'+iColumna).oninput = function(event) {
-                // var x = document.getElementById('block'+iFila+'_'+iColumna).value;                
-                letras[iFila][iColumna] = event.target.value;
-                console.log(letras);
-                };                
+    // function inicio(){
+    //     for (let iFila = 0; iFila < 6; iFila++) {
+    //         for (let iColumna = 0; iColumna < 5; iColumna++){
+    //             document.getElementById('block'+iFila+'_'+iColumna).oninput = function(event) {
+    //             // var x = document.getElementById('block'+iFila+'_'+iColumna).value;                
+    //             letras[iFila][iColumna] = event.target.value;
+    //             console.log(letras);
+    //             };                
      
-            }
-        }
-    }
+    //         }
+    //     }
+    // }
 
     /*Funcion para obtener la fila*/
     function inicio2(){        
@@ -153,6 +174,7 @@ window.onload = function(){
         }
     }
 
+    /*OBTIENE LAS LETRAS DE LA FILA Y CARGA EN EL ARRAY DE LETRAS*/
     function cargarLetras(){
         for (let iFila = 0; iFila < 6; iFila++) {
             for (let iColumna = 0; iColumna < 5; iColumna++){
@@ -161,6 +183,7 @@ window.onload = function(){
         }
     }
 
+    /*CARGA EL ARRAY DE LA FILA PARA SU COMPARACION*/
     function cargarFila(fila){
         let iFila = fila
         // var array1 = [' ',' ',' ',' ',' '];    
@@ -170,19 +193,27 @@ window.onload = function(){
         return arrayFila;
     }
 
+    /*CARGA LA PALABRA GANADORA EN EL ARRAY DE PALABRA GANADORA*/
+    //////////ESTA FUNCION DEBE SER ADAPTADA PARA INCLUIR LA ULTIMA PALABRA DEL OBJETO/////////////////////////// 
+    function cargarArrayPalabraGanadora(){
+        arrayPalabraGanadora = palabraGanadora.split("");
+        for (let i = 0; i <5 ; i++){
+            console.log(arrayPalabraGanadora[i])
+        }
+    }
+
     /*Verificación que las letras corresponden a la palabra y si estan desordenadas*/
     function validarLetra(fila){
-        let iFila = fila;
-        
+        let iFila = fila;        
         for (let i = 0; i < 5; i++){
             let letraExiste = false; 
             var contador = 0;
-            if(arrayFila[i] == arrayPalabra[i]){
+            if(arrayFila[i] == arrayPalabraGanadora[i]){
                 colores[iFila][i] = 1;
             }
             else{
                 for(let e = 0; e < 5; e++){
-                    if(arrayFila[i] == arrayPalabra[e]){
+                    if(arrayFila[i] == arrayPalabraGanadora[e]){
                         letraExiste = true;
                     }
                 }
